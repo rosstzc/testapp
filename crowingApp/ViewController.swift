85202d74e262c3f01055d0d918c955823d8fa69a
a a a a a <aaaaa@macpro-mike.local>
2015-08-28 21:06:14 +0800

    Initial Commit

---
+++ crowingApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  crowingApp
+//
+//  Created by a a a a a on 15/8/28.
+//  Copyright (c) 2015年 mike公司. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
